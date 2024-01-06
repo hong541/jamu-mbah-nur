364dddc2f9cf9163a7e13d843267ed2275a91320
hong541 <jabarmaulana7@gmail.com>
2024-01-06 17:13:51 +0700

    commit first

--- src/app.js
+++ src/app.js
@@ -1,11 +1,43 @@
 document.addEventListener("alpine:init", () => {
   Alpine.data("products", () => ({
     items: [
-      { id: 1, name: "Cengkeh", img: "1.jpg", price: 25000 },
-      { id: 2, name: "Jintan", img: "2.jpg", price: 35000 },
-      { id: 3, name: "Kayu Manis", img: "3.jpg", price: 15000 },
-      { id: 4, name: "Bunga Lawang", img: "4.jpg", price: 45000 },
-      { id: 5, name: "Kunir Bubuk", img: "5.jpg", price: 65000 },
+      {
+        id: 1,
+        name: "Cengkeh",
+        img: "1.jpg",
+        price: 25000,
+        desc: "Cengkih atau cengkeh (Syzygium aromaticum) adalah kuncup bunga kering beraroma dari keluarga pohon Myrtaceae. Cengkih adalah tanaman asli Indonesia, banyak digunakan sebagai bumbu masakan pedas di negara-negara Eropa, dan sebagai bahan utama rokok kretek khas Indonesia. ",
+      },
+
+      {
+        id: 2,
+        name: "Jintan",
+        img: "2.jpg",
+        price: 35000,
+        desc: "Jintan (Trachyspermum roxburghianum syn. Carum roxburghianum) merupakan tumbuhan menjalar yang bijinya dapat digunakan sebagai rempah-rempah dan obat-obatan.",
+      },
+
+      {
+        id: 3,
+        name: "Kayu Manis",
+        img: "3.jpg",
+        price: 15000,
+        desc: "Kayu manis atau dengan nama ilmiah Cinnamomum ialah jenis pohon penghasil rempah-rempah. di dalam kamus Biologi, Cinnamomum zeylanicum Termasuk ke dalam jenis rempah-rempah yang dihasilkan dari kulit bagian dalam yang kering, yang amat beraroma, manis, dan pedas.",
+      },
+      {
+        id: 4,
+        name: "Bunga Lawang",
+        img: "4.jpg",
+        price: 45000,
+        desc: "Bunga lawang atau Kembang Lawang atau pekak adalah rempah yang memiliki rasa yang mirip dengan Adas manis. Rempah ini memiliki nama ilmiah Illicium verum. Bunga lawang merupakan bunga yang berwarna cokelat gelap dan terdiri atas delapan sisi dengan rasa manis.",
+      },
+      {
+        id: 5,
+        name: "Kunir Bubuk",
+        img: "5.jpg",
+        price: 65000,
+        desc: "Kunyit atau kunir, (Curcuma longa Linn. syn. Curcuma domestica Val.), adalah termasuk salah satu tanaman rempah-rempah dan obat asli dari wilayah Asia Tenggara. Kemudian tanaman rempah ini, digunakan sebagai pelengkap bumbu masakan, jamu atau obat untuk menjaga kesehatan dan kecantikan seperti pemakaian dalam perawatan kulit dan wajah.",
+      },
     ],
   }));
 
